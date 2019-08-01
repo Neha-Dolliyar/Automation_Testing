bb4885a3344100b71849405cdb11be259361b129
Neha-Dolliyar <kambojneha36@gmail.com>
2019-08-01 08:52:09 +0530

    Second Commit

--- UniqueID/src/test/java/MainMethod/main.java
+++ UniqueID/src/test/java/MainMethod/main.java
@@ -1,6 +1,5 @@
 package MainMethod;
 
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -8,41 +7,36 @@ import org.testng.annotations.BeforeTest;
 
 import InDesignprojectNew.UniqueID.uploadjobtestcases;
 
+public class main extends uploadjobtestcases {
 
-public class main extends uploadjobtestcases  {
+	@BeforeTest // TESTNG Annotation
+	public void Test() throws Exception {
 
-	@BeforeTest
-	public void Test() throws Exception
-	{	
-		
 		Uploadjob();
-	
-	}	
-	
-	// For importing the job 
-    
+
+	}
+
+	// For importing the job
+
 	@org.testng.annotations.Test
-	public void ImportjobOne() throws InterruptedException
-	{
+	public void ImportjobOne() throws InterruptedException {
 		WebElement myelement = driver.findElement(By.id("image_src"));
 		Actions actions = new Actions(driver);
 		actions.moveToElement(myelement).click().build().perform();
 		myelement.sendKeys("C:\\Users\\Naehas\\Documents\\Slug Editor Jobs\\Test Automation.zip");
 		Thread.sleep(100000);
-		WebElement E2=driver.findElement(By.xpath("/html/body/div/div/div/div[1]/button/span"));
-		WebElement E1=driver.findElement(By.xpath("//*[@class='modal-title']"));
+		WebElement E2 = driver.findElement(By.xpath("/html/body/div/div/div/div[1]/button/span"));
+		WebElement E1 = driver.findElement(By.xpath("//*[@class='modal-title']"));
 		if (E1.isDisplayed()) // For clicking on Warning popup if present
 		{
 			E2.click();
-			
-		}
-		else
-		{
+
+		} else {
 			System.out.println("No popup");
 		}
-	
+
 	}
-	
+
 //	public void Login() throws InterruptedException
 //	{
 //		
